fc64c8120a0cb230f751e05c7f71476a06e6ecc3
Yassine-Hajib <yassin.pc12@gmail.com>
2026-04-19 21:56:13 +0100

    Modify The Structure

--- src/backend-java/main/java/com/worksight/model/User.java
+++ src/backend-java/main/java/com/worksight/model/User.java
@@ -1,8 +1,8 @@
-package com.worksight.model;
-
 public class User {
     private int id;
-    private String name;
     private String email;
     private String password;
+    private String role;
+
+    // getters & setters
 }
